665feda1ab33e775b97b843474d9b10bf313f99b
sdlong <sdlong.jeng@gmail.com>
2013-12-28 09:49:11 +0800

    set devise done

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20131227233734) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
+    t.string   "name",                   default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
