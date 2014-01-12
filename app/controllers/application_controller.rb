2f88e70b124aa12f164d1b55d4660e84ce52a8a9
sdlong <sdlong.jeng@gmail.com>
2014-01-12 16:06:48 +0800

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,25 +5,25 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def login_required
-  	if current_user.blank?
-  		respond_to do |format|
-  			format.html {
-  				authenticate_user!
-  			}
-  			format.js {
-  				render :partial => "common/not_logined"
-  			}
-  			format.all {
-  				head(:unauthorized)
-  			}
-  		end
-  	end
+    if current_user.blank?
+      respond_to do |format|
+        format.html {
+          authenticate_user!
+        }
+        format.js {
+          render :partial => "common/not_logined"
+        }
+        format.all {
+          head(:unauthorized)
+        }
+      end
+    end
   end
 
 
-    protected
+  protected
 
-    def configure_permitted_parameters 
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password,:password_confirmation)} 
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password,:password_confirmation)}
+  end
 end
