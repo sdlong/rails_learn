779b2d17df28e91cd3c2513d3700691cd115dcd2
sdlong <sdlong.jeng@gmail.com>
2014-01-14 09:03:45 +0800

    test email function

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 RailsLearn::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'Jung',
+  :password => '2raixdol',
+  :domain => 'rorec.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
