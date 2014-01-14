11cd8394c0a51b92cfc97b94418b488b991539e8
sdlong <sdlong.jeng@gmail.com>
2014-01-14 21:16:47 +0800

    use ibutton to apply font-awesome

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,4 +4,12 @@ module PagesHelper
     link_to(page.title_for_preview, page.title_for_preview, :class => "index-title")
   end
 
+  def render_page_read_more_link(page)
+    link_to("Read more &gt;", page_path(page), :class => "btn btn-success") 
+  end
+
+  def render_page_editable_link(page)
+
+    ibutton("Edit", edit_page_path(page), :class => "bt btn-info", :iclass => "icon-pencil")
+  end
 end
